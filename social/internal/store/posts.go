a974e9a8b29774693f4578e299dce61b1f0fd7cc
robertrahardja <robertrahardja@gmail.com>
2025-06-16 12:24:24 +0800

    added add_version_posts migration

--- social/internal/store/posts.go
+++ social/internal/store/posts.go
@@ -9,15 +9,15 @@ import (
 )
 
 type Post struct {
-	ID        int64    `json:"id"`
-	Content   string   `json:"content"`
-	Title     string   `json:"title"`
-	UserID    int64    `json:"user_id"`
-	Tags      []string `json:"tags"`
-	CreatedAt string   `json:"created_at"`
-	UpdatedAt string   `json:"updated_at"`
-	// Version   int       `json:"version"`
-	Comments []Comment `json:"comments"`
+	ID        int64     `json:"id"`
+	Content   string    `json:"content"`
+	Title     string    `json:"title"`
+	UserID    int64     `json:"user_id"`
+	Tags      []string  `json:"tags"`
+	CreatedAt string    `json:"created_at"`
+	UpdatedAt string    `json:"updated_at"`
+	Version   int       `json:"version"`
+	Comments  []Comment `json:"comments"`
 	// User      User      `json:"user"`
 }
 
